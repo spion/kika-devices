6539b8f9ff7d4f00261fa04821eac19a59b53d16
Gorgi Kosev <gorgi.kosev@gmail.com>
2012-04-12 21:33:58 +0200

    wooo toplist

--- couchapp/updates/devices.js
+++ couchapp/updates/devices.js
@@ -1,16 +1,26 @@
 function(doc, req) {
-	var filtered = ['dvoarak']
+	var filteredNames = ['dvoarak'],
+	    filteredMacs  = [],  
+	    filteredCount = 0;
 	var rip = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
 	var arplines = req.body.split("\n"), deviceNames = [];
 	for (var k = 1; k < arplines.length; ++k) {
-		var deviceName = arplines[k].split(" ")[0].replace(".local","");
-		if (!rip.test(deviceName) && deviceName.length > 0 && filtered.indexOf(deviceName) == -1) {
-			deviceNames.push(deviceName);
+		var arpline = arplines[k].split(/\s{2,}/);
+		var deviceName = arpline[0].replace(".local","");
+		var deviceMac = arpline[1];
+		if (filteredNames.indexOf(deviceName) == -1
+		  && filteredMacs.indexOf(deviceMac) == -1) {
+			if (!rip.test(deviceName) && deviceName.length > 0) {
+				deviceNames.push(deviceName);
+			}
+		}
+		else {
+			++filteredCount;
 		}
 	}
 	var json = {
 		_id: new Date().getTime().toString(),
-		count: arplines.length - 2,
+		count: arplines.length - 2 - filteredCount,
 		devices: deviceNames
 	};
 	return [json, "OK"];
