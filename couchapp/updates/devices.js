f649769f4d920eca3ef160803f710c0e518880cd
spion <gorgi.kosev@gmail.com>
2012-04-13 13:14:35 +0200

    fixed graph range, made updates/devices.js cleaner

--- couchapp/updates/devices.js
+++ couchapp/updates/devices.js
@@ -1,26 +1,24 @@
 function(doc, req) {
 	var filteredNames = ['dvoarak'],
 	    filteredMacs  = [],  
-	    filteredCount = 0;
+	    deviceCount = 0;
 	var rip = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
 	var arplines = req.body.split("\n"), deviceNames = [];
 	for (var k = 1; k < arplines.length; ++k) {
 		var arpline = arplines[k].split(/\s{2,}/);
+		if (arpline.length <= 1) continue;
 		var deviceName = arpline[0].replace(".local","");
-		var deviceMac = arpline[1];
-		if (filteredNames.indexOf(deviceName) == -1
-		  && filteredMacs.indexOf(deviceMac) == -1) {
-			if (!rip.test(deviceName) && deviceName.length > 0) {
-				deviceNames.push(deviceName);
-			}
-		}
-		else {
-			++filteredCount;
+		var deviceMac = arpline.length > 2 ? arpline[2] : null;
+		if (filteredNames.indexOf(deviceName) > -1
+		  || filteredMacs.indexOf(deviceMac) > -1) continue;
+		++deviceCount;
+		if (!rip.test(deviceName)) {
+			deviceNames.push(deviceName);
 		}
 	}
 	var json = {
 		_id: new Date().getTime().toString(),
-		count: arplines.length - 2 - filteredCount,
+		count: deviceCount,
 		devices: deviceNames
 	};
 	return [json, "OK"];
