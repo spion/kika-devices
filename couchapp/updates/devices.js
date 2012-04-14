cadc4376332298fe918735f1ff4fb17a13644dea
spion <gorgi.kosev@gmail.com>
2012-04-14 16:56:20 +0200

    fixed counter bug

--- couchapp/updates/devices.js
+++ couchapp/updates/devices.js
@@ -1,7 +1,7 @@
 function(doc, req) {
 	var filteredNames = ['dvoarak'],
 	    filteredMacs  = [],  
-	    deviceCount = 0, deviceK = 0;
+	    deviceCount = 0;
 	var rip = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
 	var arplines = req.body.split("\n"), deviceNames = [];
 	for (var k = 1; k < arplines.length; ++k) {
@@ -9,13 +9,16 @@ function(doc, req) {
 		if (arpline.length <= 1) continue;
 		var deviceName = arpline[0].replace(".local","");
 		var deviceMac = arpline.length > 2 ? arpline[2] : null;
+		// if the device is in the filtered names or filtered macs
+		// or is a regular IP instead of a hostname, don't count it
 		if (filteredNames.indexOf(deviceName) > -1
-		  || filteredMacs.indexOf(deviceMac) > -1) continue;
+		  || filteredMacs.indexOf(deviceMac) > -1
+		  || rip.test(deviceName)) continue;
+		// otherwise count it
 		++deviceCount;
-		if (!rip.test(deviceName)) {
-			//deviceNames.push(deviceName);
-			deviceNames.push("device" + (++deviceK));
-		}
+
+		// and add it to the list (removed due to privacy concerns)
+		//deviceNames.push(deviceName);
 	}
 	var json = {
 		_id: new Date().getTime().toString(),
