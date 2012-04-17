67ff2b571d9115a50493c3cee910fe55c6bc3221
Gorgi Kosev <gorgi.kosev@gmail.com>
2012-04-17 02:32:37 +0200

    more robust updateDevicesNode.sh - will  not work while other is in progress

--- router/updateDevicesNode.sh
+++ router/updateDevicesNode.sh
@@ -1,3 +1,17 @@
-iface=`cat .config-iface`
-pushurl=`cat .config-url`
-arp -ni $iface | awk '{print $3}' | grep ':' | curl $pushurl --data-binary @- -H"Content-Type:text/plain"
+
+
+lockfile='.lockfile'
+date >> log
+if [ ! -e $lockfile ]
+then
+   touch $lockfile
+   iface=`cat .config-iface`
+   pushurl=`cat .config-url`
+   arp -ni $iface | awk '{print $3}' | grep ':' | curl $pushurl --data-binary @- -H"Content-Type:text/plain"  critical-section
+   rm $lockfile
+   echo "Successful update"
+else
+   echo "attempting to push macs while previous push in progress" >> log
+fi
+
+
