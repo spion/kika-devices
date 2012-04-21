1d2f9218b7849ab3288ecac91bca323df84257e8
spion <gorgi.kosev@gmail.com>
2012-04-21 22:17:59 +0300

    automatic basePath. removed unnecessary tempfile usage.

--- router/updateDevicesNode.sh
+++ router/updateDevicesNode.sh
@@ -1,10 +1,13 @@
-basePath='/root/kika-devices';
+#!/bin/bash
 
+#basePath='/root/kika-devices';
 
-echo 'ARP' > $basePath/arp
+# Automatic basePath with bash
+# Used because the cron runs
+basePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 iface=`cat $basePath/.config-iface`
 pushurl=`cat $basePath/.config-url`
-arp -ni $iface >> $basePath/arp
-cat $basePath/arp  | awk '{print $3}' | grep ':' | sort | uniq | curl $pushurl --data-binary @- -H"Content-Type:text/plain"
+arp -ni $iface | awk '{print $3}' | grep ':' | sort | uniq | curl $pushurl --data-binary @- -H"Content-Type:text/plain"
 
 
