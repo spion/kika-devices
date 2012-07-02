4d3b5eff012c3800cb5f5302a2c052055865d545
spion <gorgi.kosev@gmail.com>
2012-07-02 12:54:31 +0200

    fixed some typos

--- nodeapp/routes/admin.js
+++ nodeapp/routes/admin.js
@@ -1,6 +1,5 @@
 var mongodb = require('../models/db.js'),
     auth = require('../models/auth.js');
-<<<<<<< HEAD
 
 twitterAdmin = require('../models/twitter-admin.js');
 
@@ -10,25 +9,12 @@ module.exports = function (app) {
             res.end("Test message posted");
         });
     });
-=======
-twitterAdmin = require('../models/twitter-admin.js')
-module.exports = function (app) {
-    app.get('/admin/twitter-test', auth.admin, function (req, res) {
-        twitterAdmin.postMessage(true, function (a, b, c) {
-            console.log([a, b, c]);
-            res.end("Test message posted");
-        });
-    })
->>>>>>> 7ed6eca0b169c8f00346dd365bc2d27601c49b08
-    app.get('/admin/dbindex', auth.admin, function (req, res) {
+   app.get('/admin/dbindex', auth.admin, function (req, res) {
         var db = mongodb();
         db.counters.ensureIndex({time:1});
         db.users.ensureIndex({'twitter.id':1});
         db.users.ensureIndex({id:1});
-<<<<<<< HEAD
         db.updates.ensureIndex({type:1, time:-1});
-=======
->>>>>>> 7ed6eca0b169c8f00346dd365bc2d27601c49b08
         res.send("OK\n");
     });
     app.get('/admin/remove', auth.admin, function (req, res) {
@@ -42,7 +28,6 @@ module.exports = function (app) {
             res.render('admin/users', {users:users});
         });
     });
-<<<<<<< HEAD
 
     app.get('/admin/migrate', auth.admin, function (req, res) {
         var hq = mongodb(process.env.MONGOHQ_URL);
@@ -57,6 +42,4 @@ module.exports = function (app) {
 
         }
     });
-=======
->>>>>>> 7ed6eca0b169c8f00346dd365bc2d27601c49b08
 };
