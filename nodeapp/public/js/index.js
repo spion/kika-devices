b5eaeca2360ccb40482307ac125c5ea915af5f31
Gorgi Kosev <gorgi.kosev@gmail.com>
2012-12-04 02:07:22 +0100

    switched to promises for speed

--- nodeapp/public/js/index.js
+++ nodeapp/public/js/index.js
@@ -7,9 +7,9 @@ function lcdOffset(num) {
 }
 
 function showLcd(where, temp, tag) {
-
+    
     temp = new Number(new Number(temp).toFixed(1));
-
+    
     $(where).html('');
     if (temp < 0) {
         temp = Math.abs(temp);
@@ -20,7 +20,7 @@ function showLcd(where, temp, tag) {
         if (k == -1)
             $("<div></div>").addClass('dot').html(".").appendTo($(where));
         $('<div></div>').appendTo($(where)).addClass('lcd')
-            .css('background-position', 0 - lcdOffset(cif));
+        .css('background-position', 0 - lcdOffset(cif));
     }
     $("<div />").addClass('tag').text(tag).appendTo($(where));
     $("<div />").addClass('clearfix').appendTo($(where));
@@ -37,75 +37,82 @@ function zeroFill(number, width) {
     return number;
 }
 
-var isPlural = function (num) {
+var isPlural = function(num) {
     return (num % 10 != 1 || num % 100 == 11);
 }
 
-var getDuration = function (dur) {
+var getDuration = function(dur) {
     var minDur = Math.ceil(dur / 1000 / 60);
     var m = minDur % 60;
     var h = Math.floor(minDur / 60);
     var ret = [];
-    if (h > 0) ret.push(h + "h");
+    if (h > 0)
+        ret.push(h + "h");
     ret.push(zeroFill(m, 2) + "m");
     return ret.join(" ");
 }
 
 var cosm = {};
 cosm.key = 'TgZljX6Yu10Bnwja0xOtSu6IXh6SAKw4UlBualJSajZXcz0g';
-cosm.feed = function (path, params, cb) {
-    if (typeof(params) == 'function') { 
+cosm.feed = function(path, params, cb) {
+    if (typeof (params) == 'function') {
         cb = params;
         params = {};
     }
-    $.ajax({
+    var ajax = {
         data: params,
-        dataType:'json',
-        url:'http://api.cosm.com/v2/feeds/' + path,
+        dataType: 'json',
+        url: 'http://api.cosm.com/v2/feeds/' + path,
         headers: {
             'X-ApiKey': cosm.key
-        },
-        success: function(data) {
-            cb(data);
-        }});
+        }
+    };
+    if (cb) ajax.success = cb;
+    return $.ajax(ajax);
 }
 
 
 
-var updateStatus = function () {
-    var plotopt = { 
-        xaxis:{ 
-            mode:"time", 
-            tickFormatter:function (val, axis) {
-                var d = new Date(val);
-                return d.toLocaleTimeString().substr(0, 5);
-            },
-        },
-        legend: {position: 'nw'}
-    };
-    cosm.feed('64655', function(data) {
-        showLcd("#temp_hw", data.datastreams[0].current_value, "hw");
-        showLcd("#temp_random", data.datastreams[3].current_value, "random");
-        showLcd("#temp_lounge", data.datastreams[1].current_value, "lounge");
+var updateStatus = function() {
+
+    var statusCosmProm = cosm.feed('86779/datastreams/hacklab_status', {
+        start: new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString()
+    }), statusAppProm  = $.getJSON("/status", {limit: 12 * 24 * 7
+    }), temperaturesProm = cosm.feed('64655', {
+        start: new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString(),limit: 300
     });
 
-    cosm.feed('86779/datastreams/hacklab_status', {
-        start: new Date(Date.now() - 1000*60*60*24).toISOString()
-    }, function(res) { 
+
+    $.when(statusCosmProm, statusAppProm, temperaturesProm).then(function(res, json, data) {
+        res = res[0]; json = json[0]; data = data[0];
+
+        var jsonApp = json;
         var json = {
-            counters: res.datapoints.map(function(dp) { 
-                return {count:dp.value - 0, time: new Date(dp.at).getTime()};  
+            counters: res.datapoints.map(function(dp) {
+                return {count: dp.value - 0,time: new Date(dp.at).getTime()};
             }).reverse()
         };
         json.counters.unshift({
             count: res.current_value - 0,
             time: new Date(res.at).getTime()
         });
+        
+        var jc_rev = json.counters.slice().reverse();
+        var changepoints = jc_rev.filter(function(el, ix) {
+            return ix < 1 || jc_rev[ix - 1].count != el.count || ix == jc_rev.length - 1;
+        });
+        var markings = changepoints.map(function(el, ix) {
+            var end = el.count == 0 && ix < changepoints.length - 1 ? changepoints[ix + 1].time : el.time;
+            return {xaxis: {from: el.time, to: end},color: "#f5f5f5"};
+        });
+
         var current = json.counters[0];
-        console.log(current);
-        var recentEnough = new Date().getTime() - current.time < 1000*60*32; // 32 minutes max
-        if (recentEnough) $("#status").html(current.count ? "отворен" : "напуштен");
-        else $("#status").html("запуштен");        
+        var recentEnough = new Date().getTime() - current.time < 1000 * 60 * 32; // 32 minutes max
+
+        if (recentEnough)
+            $("#status").html(current.count ? "отворен" : "напуштен");
+        else
+            $("#status").html("запуштен");
         var lastDate;
         for (var k = 1; k < json.counters.length; ++k) {
             var item = json.counters[k];
@@ -116,25 +123,38 @@ var updateStatus = function () {
             }
             $("#status-dur").html("повеќе од " + getDuration(new Date().getTime() - lastDate));
         }
+        var plotopt = {
+            xaxis: {
+                mode: "time",
+                tickFormatter: function(val, axis) {
+                    var d = new Date(val);
+                    return d.toLocaleTimeString().substr(0, 5);
+                },
+            },
+            legend: {position: 'nw'},
+            grid: {markings: markings}
+        };
 
+        // Plot ALL the charts!
 
-    });
-
-
-    $.getJSON("/status", { limit:12 * 24 * 7 }, function (json) {
+        var json = jsonApp;   
         var current = json.counters[0];
-        var recentEnough = new Date().getTime() - current.time < 1000*60*32;
-        if (recentEnough) $("#count").html(current.count);
-        else $("#count").html("непознат број");
-        
-        if (recentEnough || isPlural(current.count)) $("#plural").show();       
-        else $("#plural").hide();
+        var recentEnough = new Date().getTime() - current.time < 1000 * 60 * 32;
+        if (recentEnough)
+            $("#count").html(current.count);
+        else
+            $("#count").html("непознат број");
+
+        if (recentEnough || isPlural(current.count))
+            $("#plural").show();
+        else
+            $("#plural").hide();
 
         $("#names").html("");
         for (var k = 0; k < json.present.length; ++k) {
             var liItem = $("<li></li>");
-            $("<img />").attr({src:json.present[k].pic}).appendTo(liItem);
-            $("<a></a>").attr({href:'http://twitter.com/' + json.present[k].name})
+            $("<img />").attr({src: json.present[k].pic}).appendTo(liItem);
+            $("<a></a>").attr({href: 'http://twitter.com/' + json.present[k].name})
                 .html(json.present[k].name).appendTo(liItem);
             liItem.appendTo($("#names"));
         }
@@ -143,22 +163,30 @@ var updateStatus = function () {
         }
         plotData.reverse();
         $.plot($("#plot"), [plotData], plotopt);
-    });
 
-    cosm.feed('64655', {start: new Date(Date.now() - 1000*60*60*24).toISOString(), limit:300}, function(data) { 
-        var series = data.datastreams.map(function(ds) { 
+
+        showLcd("#temp_hw", data.datastreams[0].current_value, "hw");
+        showLcd("#temp_random", data.datastreams[3].current_value, "random");
+        showLcd("#temp_lounge", data.datastreams[1].current_value, "lounge");
+        var series = data.datastreams.map(function(ds) {
             return {
-                label: ds.id, 
-                data: ds.datapoints.map(function(el) {
-                    return [new Date(el.at).getTime(), el.value - 0]; 
-                }) 
-            } 
+                label: ds.id,
+            data: ds.datapoints.map(function(el) {
+                return [new Date(el.at).getTime(), el.value - 0];
+            })
+            }
         });
         $.plot($("#temps"), series, plotopt);
+
     });
 
-    window.setTimeout(updateStatus, 3 * 60 * 1000);
+    
 }
 
 updateStatus();
+
+window.setInterval(updateStatus, 3 * 60 * 1000);
+
+
+
 
